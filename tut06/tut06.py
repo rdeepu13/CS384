c9bc3def838a38e855bf4c80b0d4c721b085daca
Deepika Rajwar <rajwardeepika817@gmail.com>
2021-10-27 18:25:04 +0530

    Update tut06.py

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,25 +1,18 @@
 #Tutorial 6
-
 #Deepika Rajwar
-
 #1901EE19
 
 
 import os
-
 import re
-
 import shutil
 
-
 def regex_renamer():
-
 
 
 	# Taking input from the user
 
 	print("1. Breaking Bad")
-
 	print("2. Game of Thrones")
 
 	print("3. Lucifer")
@@ -27,31 +20,26 @@ def regex_renamer():
 
 
 	webseries_num = int(input("Enter the number of the web series that you wish to rename. 1/2/3: "))
-
 	season_padding = int(input("Enter the Season Number Padding: "))
 
 	episode_padding = int(input("Enter the Episode Number Padding: "))
 
-
 	if webseries_num==1:
 		src='./wrong_srt/Breaking Bad/'
-
 		dest='./corrected_srt/Breaking Bad/'
 	elif webseries_num==2:
 		src='./wrong_srt/Game of Thrones/'
 		dest='./corrected_srt/Game of Thrones/'
 	else:
 		src='./wrong_srt/Lucifer/'
-
 		dest='./corrected_srt/Lucifer/'
-
 
 	os.makedirs(dest)	#Create directory
 	for filename in os.listdir(src):
 		r=re.split(r'-',filename)
 
 		if webseries_num==1:
-	#no title in case of breaking bad
+ #no title in case of breaking bad
 			x=re.split(r'\D',r[1])
 
 		else:
@@ -63,16 +51,15 @@ def regex_renamer():
 		newFilename=" "
 		if webseries_num==1 and filename.endswith('mp4'):
 			newFilename=r[0]+" - Season "+season+" Episode "+ episode +".mp4"
-
 		elif webseries_num==1 and filename.endswith('srt'):
 			newFilename=r[0]+" - Season "+season+" Episode "+ episode +".srt"
 		elif filename.endswith('mp4'):
 			newFilename=r[0]+" - Season "+season+" Episode "+ episode + " - " +title[0]+".mp4"
-
 		else:
 			newFilename=r[0]+" - Season "+season+" Episode "+ episode + " - " +title[0]+".srt"
 		os.rename(src+filename,src+newFilename)
 		shutil.move(src+newFilename,dest+newFilename)
+
 
 
 
